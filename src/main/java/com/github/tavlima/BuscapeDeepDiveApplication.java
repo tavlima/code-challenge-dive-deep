7b561bc87857aed047aaa59b416b7a4728e8717d
Thiago Lima <t.augusto@gmail.com>
2016-08-05 03:15:30 -0300

    Initial commit

---
+++ src/main/java/com/github/tavlima/BuscapeDeepDiveApplication.java
@@ -0,0 +1,13 @@
+package com.github.tavlima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuscapeDeepDiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuscapeDeepDiveApplication.class, args);
+	}
+
+}
